622fdf44775c96c27eb84cf09f32c8d0872b5e49
vduarte <victorduarte2112@gmail.com>
2018-10-07 09:57:22 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,6 +15,6 @@ setup(name='mlecon',
       install_requires=[
           'tensorflow>=1.6'
       ],
-      python_requires='>3.6.4',
+      python_requires='>3.6.3',
       packages=['mlecon'],
       zip_safe=False)
