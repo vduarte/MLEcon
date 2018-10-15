3ee08ef3329922b18afe2f5802e171603d9580e0
vduarte <victorduarte2112@gmail.com>
2018-10-15 09:46:53 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mlecon',
-      version='0.1.7',
+      version='0.1.8',
       description='Machine Learning for Economic Modeling',
       url='',
       author='Victor Duarte',
