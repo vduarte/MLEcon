4c4f696a4a12bb4a43d13d7c90627827a8b71da2
vduarte <victorduarte2112@gmail.com>
2018-10-25 15:34:34 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='mlecon',
                      'mlecon_compiled.cpython-36m-x86_64-linux-gnu.so',
                      'mlecon_compiled.cp36-win_amd64.pyd']},
       install_requires=[
-          'tensorflow>=1.10',
+          'tensorflow>=1.6',
           'progressbar2',
       ],
       python_requires='>=3.6.3',
